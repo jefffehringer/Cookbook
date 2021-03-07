bd38d1dde5a9b93025504e3f0b9aa2b1fff89798
JFehringer <jeff.fehringer@gmail.com>
2021-03-07 15:37:48 -0700

    Updating works. No validation

--- src/app/pages/recipe/recipe-edit/recipe-edit.page.ts
+++ src/app/pages/recipe/recipe-edit/recipe-edit.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../services/recipe.services';
+import { ItemReorderEventDetail } from '@ionic/core';
+import { Recipe } from '@cook/store/models/recipe.interface';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -19,6 +21,28 @@ export class RecipeEditPage implements OnInit {
   ngOnInit() {
   }
 
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  reorderInstructions(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
   ionViewDidEnter() {
     const id = this.route.snapshot.paramMap.get('id');
     this.recipeService.get(id);
@@ -26,5 +50,17 @@ export class RecipeEditPage implements OnInit {
 
   ionViewDidLeave() {
     this.recipeService.get(null);
+  }
+
+  trackByIndex(index, item) {
+    return index;
+  }
+
+  trackByStep(index, item) {
+    return index;
+  }
+
+  save(recipe: Recipe) {
+    this.recipeService.update(recipe);
   }
 }
