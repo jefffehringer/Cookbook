5ecf3c1de42ffb425c9c87ad2060094c56399c37
JFehringer <jeff.fehringer@gmail.com>
2021-03-11 21:02:01 -0700

    Right pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  showRightPane = false;
   public appPages = [
     { title: 'Recipes', url: '/recipes', icon: 'pizza' },
-    { title: 'Tags', url: '/folder/Outbox', icon: 'pricetags' },
-    { title: 'Cookbooks', url: '/folder/Favorites', icon: 'book' },
-    { title: 'Whats for Dinner?', url: '/folder/Archived', icon: 'shuffle' }
+    { title: 'Tags', url: '/folder/Tags', icon: 'pricetags' },
+    { title: 'Cookbooks', url: '/folder/Cookbooks', icon: 'book' },
+    { title: 'Whats for Dinner?', url: '/folder/Dinner', icon: 'shuffle' }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(
+    private platform: Platform
+  ) {
+    this.showRightPane = this.platform.platforms().indexOf('desktop') >= 0;
+  }
 }
